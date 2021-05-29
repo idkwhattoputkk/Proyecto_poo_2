f4e9ac2220f7cd77b06fdccb2bfac4d5e779fb95
Santiago Serna <sserna1231@javerianacali.edu.co>
2021-05-29 02:47:34 -0500

    ACTUALIZACION: Completar implementacion de clase 'Jugador'
    Se corrigen algunos errores de sintaxis, y se implementan
    métodos que aún faltaban.

--- model/jugador.cpp
+++ model/jugador.cpp
@@ -1,11 +1,25 @@
 #include "jugador.h"
 
-Jugador::Jugador() {}
+const int Jugador::bolsillos = 10;
+
+Jugador::Jugador(): numItems(0)
+{
+    for(int i = 1; i <= bolsillos; ++i)
+        inventario.emplace(i, nullptr);
+}
+
+Jugador::Jugador(string nombre, int puntosVida, int x, int y): 
+    Entidad(nombre, puntosVida, x, y), Jugador() {}
+
+int Jugador::getNumItems() const
+{
+    return numItems;
+}
 
 void Jugador::mostrarInventario(){
-    map<int,Item>::iterator it = this->inventario.begin();
-    for (it ; it != inventario.end(); it++){
-        cout >> *it.first >> "    " >> it->getNombre() >> endl;
+    for (auto it = inventario.begin(); it != inventario.end(); ++it){
+        if( it->second != nullptr )
+            cout << it->first << ". " << it->second->getNombre() << endl;
     }
 }
 
@@ -13,28 +27,49 @@ void Jugador::setATK(int puntosATK){
     this->puntosATK = puntosATK;
 }
 
-void Jugador::usar(int item){
-    it = this->inventario.find(item);
-    it->usar;
+void Jugador::usar(int itemPos){
+    Item* item = inventario[itemPos];
+    item->usar();
 }
 
-void Jugador::addInvetario(Item *item){
-    if (inventario.size() < 9){ //comienza desde 1 y hasta 10 son 10 items
-        this->inventario.insert(pair<int,Item>(1,*item));
-    }
-    else:
-        cout >> "Inventario lleno" >> endl;
-
+void Jugador::addInvetario(Item* item){
+    if( numItems < bolsillos ){
+        for(auto it = inventario.begin(); it != inventario.end(); ++it)
+            if( it->second != nullptr ){
+                inventario[it->first] = item;
+                ++numItems;
+                break;
+            }
+    } else cout >> "Inventario lleno" >> endl;
 }
-int Jugador::eliminarItem(int){
-    int opcion=0;
-    mostrarInventario();
-    cout >> "Seleccione 0 para salir:" ;
-    cout >> "Selecionar el indice del item a eliminar: " ;
-    cin << opcion;
-    if (opcion == 0){
-        return 0;
-    }
-    else:
-        this->inventario.ersase(opcion);
+
+int Jugador::eliminarItem(int itemPos)
+{
+    inventario[itemPos] = nullptr;
+    --numItems;
+}
+
+void Jugador::atacar(Entidad* enemigo) override
+{
+    int opc;
+    do{
+        cout << "1. Golpe directo\n";
+        cout << "2. Usar item\n\n> ";
+        cin >> opc;
+        switch(opc){
+            case 1:
+                enemigo->setHP( enemigo->getHP() - puntosATK );
+                break;
+            case 2:
+                mostrarInventario();
+                cout << "Ingrese el item a usar\n> ";
+                cin >> opc;
+                try{
+                    Item* item = inventario.at(opc);
+                    item->usar(this, enemigo);
+                }catch(std::out_of_range& e){
+                    cout << "Item inexistente\n";
+                }
+        }
+    } while(opc != 0);
 }
