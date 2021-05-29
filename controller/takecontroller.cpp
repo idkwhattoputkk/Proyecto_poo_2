eb883a03679b9c33c35228aac1c99149e4bc0fe1
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-05-29 14:54:03 -0500

    NUEVO: takecontroller.cpp

---
+++ controller/takecontroller.cpp
@@ -0,0 +1,34 @@
+TomarController::TomarController(){
+}
+
+bool TomarController::mensaje(Mazmorra* mazmorra, int pos){
+    int opc;
+    Position* contenido = mazmorra->getContenido(pos);
+    Item* item = contenido->content;
+    item->mensaje();
+    do{
+        std::cout << "1. Sí\n";
+        std::cout << "2. No\n\n>";
+        try{
+            std::cin >> opc;
+        }catch( std::ios_base::failure &e ){
+            std::cin.clear(); //clear bad input flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+            std::cout << "Opcion invalida\n";
+            opc = 0;
+        }
+        switch(opc){
+            case 1:
+                return true;
+            case 2:
+            default:
+                return false;
+        }
+    }while(opc < 1 || opc > 2);
+}
+
+void TomarController::addInventario(Mazmorra* mazmorra, Jugador* jugador, int pos){
+    Position* contenido = mazmorra->getContenido(pos);
+    Item* item = contenido->content;
+    jugador->addInventario(item);
+}
