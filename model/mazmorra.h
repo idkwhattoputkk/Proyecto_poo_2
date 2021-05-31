9fd9fe2af7272d7d9252273db15e293332ff52d6
Santiago Serna <sserna1231@javerianacali.edu.co>
2021-05-31 00:48:14 -0500

    ACTUALIZACION: Modificar interfaz clase 'Mazmorra'
    Los cambios incluyen:
        - Añadir constantes características del mapa según dificultad
        - Añadir atributos 'listaGenerables' & 'listaExistentes'
        - Ajustar el constructor
        - Añadir métodos: 'randomPos', 'ocupado' y 'actualizarObjetos'

--- model/mazmorra.h
+++ model/mazmorra.h
@@ -1,36 +1,70 @@
 #ifdef MAZMORRA_H
 #define MAZMORRA_H
 
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <forward_list>
+#include <queue>
 #include <unordered_map>
 #include "../model/entidad.h"
 #include "../model/Item.h"
+#include "../model/jefe.h"
+#include "../model/villano.h"
+#include "../model/Arma.h"
+#include "../model/PocionAtaque.h"
+#include "../model/PocionEscape.h"
+#include "../model/PocionMuerte.h"
+#include "../model/PocionResistencia.h"
+#include "../model/PocionVida.h"
+
+//Dimensiones y numero de monstruos por dificultad
+constexpr int dimEasy = 10;
+constexpr int dimHard = 15;
+constexpr int numMonstersEasy = 25;
+constexpr int numMonstersHard = 35;
+
+constexpr int jefesEasy = 1;
+constexpr int jefesHard = 3;
+
+constexpr float percentMonstersA = 0.5;
+constexpr float percentMonstersB = 0.3;
+constexpr float percentMonstersC = 0.2;
+
+constexpr float numItems = 0.2;
 
 using std::unordered_map;
+using std::forward_list;
+using std::list;
 
 enum Tipo{
     VACIO, ENTIDAD, ITEM
 };
 
-typedef struct{
-    typedef union{
+struct Position{
+    union content{
         Entidad* entidad;
         Item* item;
-    } content;
-    Tipo tipo;
+    };
     
-} Position;
+    Tipo tipo;
+};
 
 class Mazmorra
 {
 private:
     unordered_map<int, Position*> mazmorra;
+    forward_list<Item*> listaGenerables;
+    list<Item*> listaExistentes;
 public:
     Mazmorra();
-    Mazmorra(int);
+    Mazmorra(int, int, int);
     Position* getContenido(int) const;
     void setContenido(int, Position*);
     void quitar(int);
-    bool ocupado(int);
+    int randomPos(int);
+    Tipo ocupado(int);
+    void actualizarObjetos(int);
 };
 
 #endif
