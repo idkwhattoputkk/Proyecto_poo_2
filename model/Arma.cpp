60ee8aff5bfd87fdf8d840a081e0c039fb20ee2c
Santiago Serna <sserna1231@javerianacali.edu.co>
2021-05-31 00:19:22 -0500

    ACTUALIZACION: Modificar interfaz & implementacion clase 'Arma'
    Los cambios involucran:
        - Ajustar su constructor al de la clase 'Item'
        - Sobreescribir el método 'usar' de clase 'Item'

--- model/Arma.cpp
+++ model/Arma.cpp
@@ -1,12 +1,25 @@
-
 #include "model/arma.h"
-//Variable constante y estatica que refleja el valor de arma
-const int Arma::POTENCIA  = 50;
-
 //Metodos y constructores clase arma
 Arma::Arma(){}
+
+Arma::Arma(string nombre, int durabilidad, int desgaste, int frecAparicion
+    int frecDesaparicion, int potencia) : 
+    POTENCIA(potencia), Item(nombre, durabilidad, desgaste, frecAparicion
+    frecDesaparicion) {}
 
 int Arma::getPotencia() {
     return this->POTENCIA;
 }
 
+void Arma::usar(Entidad* jugador, Entidad* enemigo) override
+{
+    int puntosATK = jugador.getATK() + POTENCIA;
+    enemigo->setHP( enemigo->getHP() - puntosATK );
+    usos -= desgaste;
+    if( usos <= 0 ){
+        jugador->eliminarItem(pocket);
+        ~Arma();
+    }
+}
+
+Arma::~Arma() override {}
