b8cbb795e7059fc65270e4a51421e4a74c7dff1f
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-05-31 16:26:41 -0500

    ACTUALIZACION: Se ajustó la clase jefe
    Los cambios incluyen:
    	- Se ajustaron los constructores para que inicialicen ciertas constantes
    	- Se implementó el método soltar
    	- Se modificó 'turno', int->void

--- model/jefe.cpp
+++ model/jefe.cpp
@@ -2,27 +2,27 @@
 
 Jefe::Jefe() : recompensa(nullptr)
 {
-    listaAtaques.push_back( Ataque("Rayo Tolerador", foo) );
-    listaAtaques.push_back( Ataque("Gas Pimienta", foo) );
-    listaAtaques.push_back( Ataque("Rasengan", foo) );
+    listaAtaques.push_back( Ataque("Rayo Tolerador", 30) );
+    listaAtaques.push_back( Ataque("Gas Pimienta", 40) );
+    listaAtaques.push_back( Ataque("Rasengan", 50) );
 }
 
 Jefe::Jefe(string nombre, int vidaMax, int pos):
 Entidad(nombre, vidaMax, pos) {}
 
-Jefe::Jefe(string nombre, int vidaMax, int pos, Item* item): recompensa(item)
+Jefe::Jefe(string nombre, int vidaMax, int pos, Item* item): recompensa(item),
 Entidad(nombre, vidaMax, pos) {}
 
-void Jefe::turno(Entidad* enemigo) override
+void Jefe::turno(Entidad* enemigo)
 {
     srand(time(NULL));
     int posAtaque = rand() % listaAtaques.size();
     Ataque atk = listaAtaques[posAtaque];
-    std::cout << getName() << " usa " << atk.getNombre() << std::endl;
+    std::cout << getName() << " usa " << atk.getName() << std::endl;
     enemigo->setHP( enemigo->getHP() - atk.getPotencia() );
 }
 
-Item* Jefe::soltar() override
+Item* Jefe::soltar()
 {
     return recompensa;
 }
